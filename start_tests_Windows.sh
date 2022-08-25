629d6a2f7b859479a3e47dd83571f0188ae66eff
ALTOM-PC28\sandrina.sere <sandrina.sere@altom.com>
2022-08-25 15:45:38 +0300

    change pytest call from sh file

--- start_tests_Windows.sh
+++ start_tests_Windows.sh
@@ -1,7 +1,7 @@
 start "" "App\TrashCatWindows\TrashCatWindows.exe"
 
-python -m pytest TestsPython\\tests\\main_menu_page_test.py
-python -m pytest TestsPython\\tests\\game_play_test.py
-python -m pytest TestsPython\\tests\\start_page_test.py
+pytest TestsPython\\tests\\main_menu_page_test.py
+pytest TestsPython\\tests\\game_play_test.py
+pytest TestsPython\\tests\\start_page_test.py
 
 tskill TrashCatWindows
