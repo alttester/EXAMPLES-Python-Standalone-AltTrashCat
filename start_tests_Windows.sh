3e2869e8856d07a74fb5850cfa6f9417b40fd568
ALTOM-PC28\sandrina.sere <sandrina.sere@altom.com>
2022-08-26 11:43:11 +0300

    add dos2unix for sh file

--- start_tests_Windows.sh
+++ start_tests_Windows.sh
@@ -1,8 +1,8 @@
 start "" "App\TrashCatWindows\TrashCatWindows.exe"
 
-python -i pytest TestsPython\\tests\\main_menu_page_test.py
-python -i pytest TestsPython\\tests\\game_play_test.py
-python -i pytest TestsPython\\tests\\start_page_test.py
+python -m pytest TestsPython\\tests\\main_menu_page_test.py
+python -m pytest TestsPython\\tests\\game_play_test.py
+python -m pytest TestsPython\\tests\\start_page_test.py
 
 
 tskill TrashCatWindows
