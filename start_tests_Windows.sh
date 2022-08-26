52fb5bc05a910dad2d0238522ad52391ec0db86d
ALTOM-PC28\sandrina.sere <sandrina.sere@altom.com>
2022-08-26 12:02:04 +0300

    change back to bash for sh file

--- start_tests_Windows.sh
+++ start_tests_Windows.sh
@@ -4,5 +4,4 @@ python -m pytest TestsPython\\tests\\main_menu_page_test.py
 python -m pytest TestsPython\\tests\\game_play_test.py
 python -m pytest TestsPython\\tests\\start_page_test.py
 
-
 tskill TrashCatWindows
